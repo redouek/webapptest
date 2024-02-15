f928a247d25a6339a1ea1665aed362d8cd6091bc
Douek <douekweb@gmail.com>
2024-02-15 03:04:54 -0300

    Update script.js

--- saliva/js/script.js
+++ saliva/js/script.js
@@ -1,4 +1,10 @@
-// Lógica JavaScript para interatividade do site
+// Script para salvar como PDF
 function salvarPDF() {
-    // Lógica para salvar o relatório como PDF
+    // Lógica para salvar como PDF
+    alert("Função de salvar PDF ainda não implementada.");
+}
+
+// Script para imprimir página
+function imprimir() {
+    window.print();
 }
